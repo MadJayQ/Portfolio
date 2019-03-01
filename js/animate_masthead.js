ecae8799624a34bdd240620670c6c739fa8ae686
Jake <jthornton3030@floridapoly.edu>
2019-03-01 18:35:20 -0500

    Initial Commit

---
+++ js/animate_masthead.js
@@ -0,0 +1,9 @@
+(function($) {
+    "use strict"; // Start of use strict
+
+    var animateMasthead = function() {
+        $("#subtitle").addClass('animated bounceOutLeft');
+    }
+
+    animateMasthead();
+})(jQuery);
